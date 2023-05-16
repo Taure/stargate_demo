a05f66c4bc9c9646e96eba761520e4d15c7cabc9
Daniel Widgren <daniel.widgren@gmail.com>
2023-05-16 09:46:22 +0200

    init

---
+++ src/stargate_demo_router.erl
@@ -0,0 +1,10 @@
+-module(stargate_demo_router).
+-behaviour(nova_router).
+
+-export([
+         routes/1
+        ]).
+
+%% The Environment-variable is defined in your sys.config in {nova, [{environment, Value}]}
+routes(_Environment) ->
+    stargate_router:create_routes(stargate_demo).
